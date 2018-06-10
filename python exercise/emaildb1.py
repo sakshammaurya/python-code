1ada33189e91af7b471f3a0c9f8d0cd08c2cfb51
sakshammaurya <sammaurya843@gmail.com>
2018-06-10 21:48:18 +0530

    pyhon codes
    python codes for begineers

---
+++ python exercise/emaildb1.py
@@ -0,0 +1,37 @@
+import sqlite3
+
+conn = sqlite3.connect('email2db.sqlite')
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Counts')
+
+cur.execute('''
+CREATE TABLE Counts (org TEXT, count INTEGER)''')
+
+fname = input('Enter file name: ')
+if (len(fname) < 1): fname = 'mbox.txt'
+fh = open(fname)
+for line in fh:
+    if not line.startswith('From: '): continue
+    pieces = line.split()
+    c = pieces[1].split("@")
+    org=c[0]
+    
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (org,))
+    row = cur.fetchone()
+    if row is None:
+        cur.execute('''INSERT INTO Counts (org, count)
+                VALUES (?, 1)''', (org,))
+    else:
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
+                    (org,))
+conn.commit()
+
+# https://www.sqlite.org/lang_select.html
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
+c=0
+for row in cur.execute(sqlstr):
+    c=c+row[1]
+    print(str(row[0]), row[1])
+print('c',c)
+cur.close()
---
+++ python exercise/gmane/gline.js
@@ -0,0 +1,5 @@
+gline = [ ['year','umich.edu','indiana.edu','berkeley.edu','ucdavis.edu','uct.ac.za','mac.com','gmail.com','yale.edu','columbia.edu','cam.ac.uk'],
+['01',114,32,33,37,27,14,23,11,28,19],
+['02',110,29,20,16,20,34,22,38,9,23],
+['12',57,12,12,11,14,12,10,5,13,7]
+];
